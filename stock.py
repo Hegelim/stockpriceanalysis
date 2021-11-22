ee6719d3259654c417baa0349fbf8ff364e45432
Yewen Zhou <yewenzhou@berkeley.edu>
2021-11-21 20:26:12 -0500

    Could save files

--- stock.py
+++ stock.py
@@ -1,22 +1,19 @@
 from airflow import DAG
-from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 
 from datetime import datetime, timedelta
-import datetime as dt
-import pandas as pd
 import yfinance as yf
-import requests
-import lxml
-from functools import reduce
 import pickle
+import pandas as pd
+from sklearn.linear_model import LinearRegression
 
+COMPANIES = ['AAPL', 'GOOGL', 'FB', 'MSFT', 'AMZN']
+DF_DIRECTORY = "hw4/stock_dfs.pkl"
+MODELS_DIRECTORY = "hw4/models.pkl"
+FEATURE_COLS = ["Open", "Low", "Close", "Volume"]
+PREDICTED_COL = ["High"]
+RELATIVE_ERRORS_DIREC = "hw4/relative_errors.csv"
 
-TICKERS = ['AAPL', 'GOOGL', 'FB', 'MSFT', 'AMZN']
-
-############################################
-# DEFINE AIRFLOW DAG (SETTINGS + SCHEDULE)
-############################################
 default_args = {
     'owner': 'yewen',
     'depends_on_past': False,
@@ -27,21 +24,58 @@ default_args = {
 }
 
 
-def get_data_func():
+def get_date_today():
+    """Get date today in str format such as 20201119. """
+    return datetime.today().strftime('%Y%m%d')
+
+
+def get_new_data():
+    """Read in models, read in csv, get new data, calculate relative errors,
+    update csv. """
+    first_day = False
+    try:
+        with open(MODELS_DIRECTORY, 'rb') as f:
+            models = pickle.load(f)
+
+        relative_errors = pd.read_csv(RELATIVE_ERRORS_DIREC, index_col=0)
+    except FileNotFoundError:
+        print("No files for models.")
+        first_day = True
+        pass
+
     stock_dfs = []
-    for task in TICKERS:
-        df = yf.download(task, period='10d')
+    for i, company in enumerate(COMPANIES):
+        df = yf.download(company, period='10d')
+        if not first_day:
+            model = models[i]
+            X = df[FEATURE_COLS].to_numpy()
+            # high_today = df.loc[get_date_today(), 'High']
+            high_today = df.iloc[-1, :]['High']
+            relative_error = (model.predict(X) - high_today) / high_today
+            relative_errors[get_date_today()] = relative_error
+            relative_errors.to_csv(RELATIVE_ERRORS_DIREC)
+        else:
+            # intialize the csv file
+            relative_errors = pd.DataFrame()
+            relative_errors.to_csv(RELATIVE_ERRORS_DIREC)
         stock_dfs.append(df)
-    with open('stock_dfs.pkl', 'wb') as f:
+    with open(DF_DIRECTORY, 'wb') as f:
         pickle.dump(stock_dfs, f)
 
 
-def receive_data_func():
-    with open('stock_dfs.pkl', 'rb') as f:
+def make_prediction():
+    """Read in the data, make 5 Linear Regression models, save to file. """
+    with open(DF_DIRECTORY, 'rb') as f:
         dfs = pickle.load(f)
+    models = []
     for df in dfs:
-        print(df.head())
-        print(f"Type: {type(df)}")
+        reg = LinearRegression()
+        X = df[FEATURE_COLS].to_numpy()
+        y = df[PREDICTED_COL].to_numpy()
+        reg.fit(X, y)
+        models.append(reg)
+    with open(MODELS_DIRECTORY, 'wb') as f:
+        pickle.dump(models, f)
 
 
 with DAG('stock',
@@ -51,12 +85,11 @@ with DAG('stock',
          start_date=datetime(2021, 1, 1),
          schedule_interval=timedelta(minutes=1)
          ) as dag:
-
     get_data_task = PythonOperator(task_id='get_data',
-                                   python_callable=get_data_func)
+                                   python_callable=get_new_data)
 
-    receive_data_task = PythonOperator(task_id=f"receive_data",
-                                       python_callable=receive_data_func)
+    make_prediction_task = PythonOperator(task_id=f"receive_data",
+                                          python_callable=make_prediction)
 
     # task dependencies
-    get_data_task >> receive_data_task
+    get_data_task >> make_prediction_task
--- stocktry.py
+++ stocktry.py
@@ -2,10 +2,31 @@ import pandas as pd
 import yfinance as yf
 from datetime import date, timedelta
 from sklearn.linear_model import LinearRegression
+from datetime import datetime
+
+FEATURE_COLS = ["Open", "Low", "Close", "Volume"]
+PREDICTED_COL = ["High"]
+
+def get_date_today():
+    """Get date today in str format such as 20201119. """
+    return datetime.today().strftime('%Y%m%d')
 
 if __name__ == '__main__':
-    tickers = ['AAPL', 'GOOGL', 'FB', 'MSFT', 'AMZN']
-    for i in range(len(tickers)):
-        df = yf.download(tickers[i],
-                         period='10d')
-        print(df)
+    # tickers = ['AAPL', 'GOOGL', 'FB', 'MSFT', 'AMZN']
+    # for i in range(len(tickers)):
+    #     df = yf.download(tickers[i],
+    #                      period='10d')
+    #     print(df)
+    #     print(df.loc['20211119', 'High'])
+        # reg = LinearRegression()
+        # X = df[FEATURE_COLS].to_numpy()
+        # y = df[PREDICTED_COL].to_numpy()
+        # reg.fit(X, y)
+        # print(y)
+        # print(reg.predict(X))
+    # print(get_date_today())
+    # df = yf.download("GOOGL", period='10d')
+    # print(df.iloc[-1, :]['High'])
+    df = pd.DataFrame()
+    df[get_date_today()] = 2934.2
+    print(df)
